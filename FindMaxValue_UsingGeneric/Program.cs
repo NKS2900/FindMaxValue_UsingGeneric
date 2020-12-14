39f14de3cbc78b91bb3eb4cee8a29ac302504719
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-14 19:23:39 +0530

    [NIJAM] Add:Welcome TO Find_Max_Number Using Generic

---
+++ FindMaxValue_UsingGeneric/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxValue_UsingGeneric
+{
+    public class Program
+    { 
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO Find_MaxValue_UsingGenerics");          
+            
+        }
+    }
+}
