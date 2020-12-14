89c65b86d04a2c38dbc6681ac80fb8f78d3b7e00
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-14 21:44:24 +0530

    [NIJAM] REFACTOR:Creating GenericClass to take Generic Type.

---
+++ FindMaxValTesting/UnitTest2.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindMaxValue_UsingGeneric;
+namespace FindMaxValTesting
+{
+    [TestClass]
+    public class UnitTest2
+    {
+
+        /// <summary>
+        /// Givens the maximum number first position.
+        /// </summary>        
+        [TestMethod]
+        public void GivenMaxNumber_FirstPosition()
+        {
+            int result = GenricMaximum<int>.testMaximum(40, 20, 30);
+            int maxNumber = 40;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Givens the maximum number second position.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxNumber_SecondPosition()
+        {
+            int result = GenricMaximum<int>.testMaximum(10, 40, 30);
+            int maxNumber = 40;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Givens the maximum number third position.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxNumber_ThirdPosition()
+        {
+            int result = GenricMaximum<int>.testMaximum(10, 20, 40);
+            int maxNumber = 40;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Given the maximum float number first position.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFloatNumber_FirstPosition()
+        {
+            double result = GenricMaximum<double>.testMaximum(55.5, 20.2, 33.3);
+            double maxNumber = 55.5;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Given the maximum float number second position.
+        /// </summary>
+        public void GivenMaxFloatNumber_SecondPosition()
+        {
+            double result = GenricMaximum<double>.testMaximum(20.2, 55.5, 33.3);
+            double maxNumber = 55.5;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Given the maximum float number third position.
+        /// </summary>
+        public void GivenMaxFloatNumber_ThirdPosition()
+        {
+            double result = GenricMaximum<double>.testMaximum(20.2, 33.3, 55.5);
+            double maxNumber = 55.5;
+            Assert.AreEqual(maxNumber, result);
+        }
+
+        /// <summary>
+        /// Given the maximum string first position.
+        /// </summary>
+        public void GivenMaxString_FirstPosition()
+        {
+            string result = GenricMaximum<string>.testMaximum("Peach", "Apple", "Banana");
+            string maxString = "Peach";
+            Assert.AreEqual(maxString, result);
+        }
+
+        /// <summary>
+        /// Given the maximum string second position.
+        /// </summary>
+        public void GivenMaxString_SecondPosition()
+        {
+            string result = GenricMaximum<string>.testMaximum("Apple", "Peach", "Banana");
+            string maxString = "Peach";
+            Assert.AreEqual(maxString, result);
+        }
+
+        /// <summary>
+        /// Given the maximum string third position.
+        /// </summary>
+        public void GivenMaxString_ThirdPosition()
+        {
+            string result = GenricMaximum<string>.testMaximum("Apple", "Banana", "Peach");
+            string maxString = "Peach";
+            Assert.AreEqual(maxString, result);
+        }
+
+    }
+}
---
+++ FindMaxValue_UsingGeneric/GenericMaximum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxValue_UsingGeneric
+{
+    public class GenricMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenricMaximum{T}"/> class.
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        public GenricMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        /// <summary>
+        /// Tests the maximum.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns></returns>
+        public static T testMaximum<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(firstValue) > 0 ||
+                secondValue.CompareTo(thirdValue) > 0 && secondValue.CompareTo(firstValue) >= 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+    }
+}
